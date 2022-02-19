3e61f81cabde363783e29d83009a8bed96fdd9e7
Arran Ubels <arran4@gmail.com>
2022-02-20 00:18:54 +1100

    Refactor

--- wrapper.go
+++ wrapper.go
@@ -10,19 +10,21 @@ type SimpleWrapper struct {
 	FoldOptions []FolderOption
 }
 
-func (s *SimpleWrapper) addFoldConfig(option FolderOption) {
-	s.FoldOptions = append(s.FoldOptions, option)
+func (sw *SimpleWrapper) addFoldConfig(option FolderOption) {
+	sw.FoldOptions = append(sw.FoldOptions, option)
 }
 
 func SimpleWrapTextToImage(text string, i *image.RGBA, grf font.Face, opts ...WrapperOption) error {
 	sw := &SimpleWrapper{}
-	for _, opt := range opts {
-		opt.ApplyWrapperConfig(sw)
-	}
-	ls, _, err := SimpleWrapTextToRect(text, i.Rect, grf, opts...)
+	sw.ApplyOptions(opts)
+	ls, _, err := sw.TextToRect(text, i.Rect, grf)
 	if err != nil {
 		return fmt.Errorf("wrapping text: %s", err)
 	}
+	return sw.TextToImage(i, ls)
+}
+
+func (sw *SimpleWrapper) TextToImage(i *image.RGBA, ls []Line) error {
 	p := i.Rect.Min
 	for _, l := range ls {
 		s := l.Size()
@@ -37,9 +39,17 @@ func SimpleWrapTextToImage(text string, i *image.RGBA, grf font.Face, opts ...Wr
 
 func SimpleWrapTextToRect(text string, r image.Rectangle, grf font.Face, opts ...WrapperOption) ([]Line, image.Point, error) {
 	sw := &SimpleWrapper{}
+	sw.ApplyOptions(opts)
+	return sw.TextToRect(text, r, grf)
+}
+
+func (sw *SimpleWrapper) ApplyOptions(opts []WrapperOption) {
 	for _, opt := range opts {
 		opt.ApplyWrapperConfig(sw)
 	}
+}
+
+func (sw *SimpleWrapper) TextToRect(text string, r image.Rectangle, grf font.Face) ([]Line, image.Point, error) {
 	ls := make([]Line, 0)
 	n := 0
 	rt := []rune(text)
@@ -47,7 +57,7 @@ func SimpleWrapTextToRect(text string, r image.Rectangle, grf font.Face, opts ..
 	for p.Y < r.Dy() {
 		l, ni, err := SimpleFolder(SimpleBoxer, grf, rt[n:], r, sw.FoldOptions...)
 		if err != nil {
-			return nil, image.Point{}, fmt.Errorf("boxing text: %w", err)
+			return nil, image.Point{}, fmt.Errorf("boxing text at pos %d: %w", n, err)
 		}
 		if l == nil {
 			break
